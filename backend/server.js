229243a3165a197f1f348a932eed1cec978470b5
Aayush-kumar-ctrl <proudindian519@gmail.com>
2026-04-30 03:16:48 +0530

    fina changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config(); // ✅ THIS IS REQUIRED
+require("dotenv").config();
 
-const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 
 const app = express();
@@ -11,15 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-// Server start
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on port " + (process.env.PORT || 5000));
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
